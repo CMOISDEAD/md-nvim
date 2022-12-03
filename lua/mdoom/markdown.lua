c36664b64817f0df514cac9b931326815e1a459e
CMOISDEAD <camilodavina@gmail.com>
2022-12-03 18:36:00 -0500

    exec

--- lua/mdoom/markdown.lua
+++ lua/mdoom/markdown.lua
@@ -1,7 +1,10 @@
 local M = {}
-function M.startMd()
+function M.startMD()
   local buf = vim.api.nvim_buf_get_name(0)
   print("Starting server with file" .. buf)
+  os.execute("mdoom-preview " .. buf)
+  -- Check if buf is a .md file
+  -- manage error and then exec
 end
 
 return M
